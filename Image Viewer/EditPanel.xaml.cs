1cddc8bec58dfa85c9244030fe2b210b9c015e0c
Ivanov <vanya101598@gmail.com>
2016-12-26 23:52:32 +0200

    First steps

--- Image Viewer/EditPanel.xaml.cs
+++ Image Viewer/EditPanel.xaml.cs
@@ -25,6 +25,8 @@ namespace Image_Viewer
         private int Angle = 0;
         private RotateTransform transformRotate = new RotateTransform(0);
         private ScaleTransform transformScale = new ScaleTransform();
+        private bool SaveState = true;
+        private double Number = 1.5;
 
         public EditPanel()
         {
@@ -35,8 +37,6 @@ namespace Image_Viewer
         {
             this.wn = w;
             this.imageName = this.wn.imageOn.Source.ToString();
-            
-            
         }
 
         public void RotateRight_Click(object sender, RoutedEventArgs e)
@@ -80,27 +80,6 @@ namespace Image_Viewer
             tb.BeginInit();
             tb.Source = bi;
             // Set image rotation.
-            if (transformScale.ScaleX == -1)
-            {
-                transformScale = new ScaleTransform() { ScaleX = 1 };
-            }
-            else
-            {
-                transformScale = new ScaleTransform() { ScaleX = -1 };
-            }
-            tb.Transform = transformScale;
-            tb.EndInit();
-            // Set the Image source.
-            this.wn.imageOn.Source = tb;
-        }
-
-        private void MirrorY_Click(object sender, RoutedEventArgs e)
-        {
-            TransformedBitmap tb = new TransformedBitmap();
-            BitmapImage bi = new BitmapImage(new Uri(this.imageName));
-            tb.BeginInit();
-            tb.Source = bi;
-            // Set image rotation.
             if (transformScale.ScaleY == -1)
             {
                 transformScale = new ScaleTransform() { ScaleY = 1 };
@@ -115,14 +94,72 @@ namespace Image_Viewer
             this.wn.imageOn.Source = tb;
         }
 
+        private void MirrorY_Click(object sender, RoutedEventArgs e)
+        {
+            TransformedBitmap tb = new TransformedBitmap();
+            BitmapImage bi = new BitmapImage(new Uri(this.imageName));
+            tb.BeginInit();
+            tb.Source = bi;
+            // Set image rotation.
+            if (transformScale.ScaleX == -1)
+            {
+                transformScale = new ScaleTransform() { ScaleX = 1 };
+            }
+            else
+            {
+                transformScale = new ScaleTransform() { ScaleX = -1 };
+            }
+            tb.Transform = transformScale;
+            tb.EndInit();
+            // Set the Image source.
+            this.wn.imageOn.Source = tb;
+        }
+
         private void ZoomOut_Click(object sender, RoutedEventArgs e)
         {
-           
-            this.wn.imageOn.Height = (this.wn.ActualHeight - 20) * 1.5;
-            this.wn.imageOn.Width = this.wn.ActualWidth * 1.5;
+            this.wn.PreviousHeight = this.wn.imageOn.ActualHeight;
+            this.wn.PreviousWidth = this.wn.imageOn.ActualWidth;
+
+            if((this.wn.PreviousHeight * 1.5 >= this.wn.HeightDef) || (this.wn.PreviousWidth * 1.5 >= this.wn.WidthDef))
+            {
+                this.wn.imageOn.Height = this.wn.imageOn.ActualHeight / Number;
+                this.wn.imageOn.Width = this.wn.imageOn.ActualWidth / Number;
+            }
+            else
+            {
+                this.wn.Scroll.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+                this.wn.Scroll.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+                this.wn.imageOn.Height = this.wn.imageOn.ActualHeight / Number;
+                this.wn.imageOn.Width = this.wn.imageOn.ActualWidth / Number;
+            }
         }
 
         private void ZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+            this.wn.PreviousHeight = this.wn.imageOn.ActualHeight;
+            this.wn.PreviousWidth = this.wn.imageOn.ActualWidth;
+            this.wn.imageOn.Height = this.wn.imageOn.ActualHeight * Number;
+            this.wn.imageOn.Width = this.wn.imageOn.ActualWidth * Number;
+
+            this.wn.Scroll.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+            this.wn.Scroll.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+        }
+
+        private void AutoSaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(SaveState)
+            {
+                SaveState = false;
+                this.ImageSave.Source = new BitmapImage(new Uri("pack://application:,,,/Image Viewer;component/image/p_autosave_off@2x.png"));
+            }
+            else
+            {
+                SaveState = true;
+                this.ImageSave.Source = new BitmapImage(new Uri("pack://application:,,,/Image Viewer;component/image/p_autosave_on@2x.png"));
+            }
+        }
+
+        private void OriginalSize_Click(object sender, RoutedEventArgs e)
         {
 
         }
--- Image Viewer/MainWindow.xaml.cs
+++ Image Viewer/MainWindow.xaml.cs
@@ -24,10 +24,75 @@ namespace Image_Viewer
         private WindowState state = WindowState.Normal;
         private int number = 0;
         private string[] files;
+        private string FileName = "";
+        //private Object data;
+
+        private double mHeightDef;
+        private double mWidthDef;
+        private double mPreviousHeight;
+        private double mPreviousWidth;
+
+        public double HeightDef
+        {
+            get
+            {
+                return this.mHeightDef;
+            }
+            set
+            {
+                this.mHeightDef = value;
+            }
+        }
+        public double WidthDef
+        {
+            get
+            {
+                return this.mWidthDef;
+            }
+            set
+            {
+                this.mWidthDef = value;
+            }
+        }
+        public double PreviousHeight
+        {
+            get
+            {
+                return this.mPreviousHeight;
+            }
+            set
+            {
+                this.mPreviousHeight = value;
+            }
+        }
+        public double PreviousWidth
+        {
+            get
+            {
+                return this.mPreviousWidth;
+            }
+            set
+            {
+                this.mPreviousWidth = value;
+            }
+        }
 
         public MainWindow()
         {
             InitializeComponent();
+
+            FileStream f = File.Open("test.jpg", FileMode.Open);
+            BitmapDecoder decoder = JpegBitmapDecoder.Create(f, BitmapCreateOptions.IgnoreColorProfile, BitmapCacheOption.Default);
+            BitmapMetadata metadata = (BitmapMetadata)decoder.Frames[0].Metadata;
+            // Получаем заголовок через поле класса
+            string title = metadata.Title;
+            // Получаем заголовок из XMP
+            string xmptitle = (string)metadata.GetQuery(@"/xmp/<xmpalt>dc:title");
+            // Получаем заголовок из EXIF
+            string exiftitle = (string)metadata.GetQuery(@"/app1/ifd/{ushort=40091}");
+            // Получаем заголовок из IPTC
+            string iptctitle = (string)metadata.GetQuery(@"/app13/irb/8bimiptc/iptc/object name");
+
         }
 
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
@@ -36,16 +101,31 @@ namespace Image_Viewer
             this.DragMove();
         }
 
+        public void SaveChanges()
+        {
+            //if (FileName != "")
+            //{
+            //    BitmapSource img = (BitmapSource)(bi);
+            //    CachedBitmap cache = new CachedBitmap(img, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+            //    TransformedBitmap tb = new TransformedBitmap(cache, new RotateTransform(Angle));
+            //    TiffBitmapEncoder encoder = new TiffBitmapEncoder();
+            //    encoder.Frames.Add(BitmapFrame.Create(tb));
+            //    using (FileStream file = File.OpenWrite(path))
+            //    {
+            //        encoder.Save(file);
+            //    }
+            //}
+        }
+
         private void Image_Drop(object sender, DragEventArgs e)
         {
-            EditPnl.Visibility = Visibility.Visible;
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
                 // Note that you can have more than one file.
                 files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                string FileName = files[0];
+                FileName = files[0];
                 
-                //object data = e.Data.GetData(typeof(Object));
+                //data = e.Data.GetData(typeof(Object));
                 //MessageBox.Show(files.Length.ToString());
                 //MessageBox.Show(FileName);
 
@@ -53,6 +133,10 @@ namespace Image_Viewer
                 firstBlock.Text = String.Empty;
                 secondBlock.Text = String.Empty;
                 this.EditPnl.Add_Window_Owner(this);
+                this.HeightDef = imageOn.ActualHeight;
+                this.PreviousHeight = imageOn.ActualHeight;
+                this.WidthDef = imageOn.ActualWidth;
+                this.PreviousWidth = imageOn.ActualWidth;
             }
         }
 
@@ -83,6 +167,16 @@ namespace Image_Viewer
         private void Minimize_Click(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
+        }
+
+        private void UserControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            EditPnl.Opacity = 1;
+        }
+
+        private void UserControl_MouseLeave(object sender, MouseEventArgs e)
+        {
+            EditPnl.Opacity = 0;
         }
     }
 }
